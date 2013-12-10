b6e8267364e71fb359b47661fdec9a80f8d6a476
Maxime FRIEH <maxime.frieh@yahoo.fr>
2013-12-10 22:38:10 +0100

    Première batterie de tests après création des constructeurs de types de compte

--- TestBanque.java
+++ TestBanque.java
@@ -3,6 +3,24 @@
  */
 public class TestBanque {
 
+	//caractéristiques réglementaires des différents livrets
+	final double tauxLA	=1.75;
+	final double tauxLDD	=1.75;
+	final double tauxLEP	=2.25;
+	
+	final int plafondLA	=15300;
+	final int plafondLDD	=6000;
+	final int plafondLEP	=7700;
+
+	final boolean accesLA	=false;
+	final boolean accesLDD	=false;
+	final boolean accesLEP	=true;
+
+	final int ressourcesLA	=0;
+	final int ressourcesLDD	=0;
+	final int ressourcesLEP	=757;
+
+
 	/**
 	 * methode verifier
 	 * comme Lanceur.assertEquals mais plus rapide a ecrire
@@ -23,9 +41,45 @@ public class TestBanque {
 		verifier(-1,c.impots, "montant des revenus incorrect");
 		verifier(10,c.comptes.length,"Erreur tableau des comptes");
 	}
-	
 	/**
-	 * lancement des tests
+	* test constructeurs des types de compte
+	*/
+	public void testTypesDeComptes(){
+		//Compte courant
+		TypeCompte CC = new TypeCompte();
+		
+		verifier(0.0,CC.taux, "taux incorrect");
+		verifier(false,CC.plafondDepot, "Condition de plafond incorrecte");
+		verifier(0,CC.montantPlafond, "montant plafond incorrect");
+		verifier(false,CC.accesCompte, "Condition d'accès incorrecte");
+		verifier(0,CC.montantRessources,"montant de ressources incorrect");
+
+		TypeCompte LA = new TypeCompte(tauxLA,true,plafondLA,accesLA,ressourcesLA);
+		
+		verifier(tauxLA,LA.taux, "taux incorrect");
+		verifier(true,LA.plafondDepot, "Condition de plafond incorrecte");
+		verifier(plafondLA,LA.montantPlafond, "montant plafond incorrect");
+		verifier(accesLA,LA.accesCompte, "Condition d'accès incorrecte");
+		verifier(ressourcesLA,LA.montantRessources,"montant de ressources incorrect");
+
+		TypeCompte LDD = new TypeCompte(tauxLDD,true,plafondLDD,accesLDD,ressourcesLDD);
+		
+		verifier(tauxLDD,LDD.taux, "taux incorrect");
+		verifier(true,LDD.plafondDepot, "Condition de pLDDfond incorrecte");
+		verifier(plafondLDD,LDD.montantPlafond, "montant plafond incorrect");
+		verifier(accesLDD,LDD.accesCompte, "Condition d'accès incorrecte");
+		verifier(ressourcesLDD,LDD.montantRessources,"montant de ressources incorrect");
+
+		TypeCompte LEP = new TypeCompte(tauxLEP,true,plafondLEP,accesLEP,ressourcesLEP);
+		
+		verifier(tauxLEP,LEP.taux, "taux incorrect");
+		verifier(true,LEP.plafondDepot, "Condition de plafond incorrecte");
+		verifier(plafondLEP,LEP.montantPlafond, "montant plafond incorrect");
+		verifier(accesLEP,LEP.accesCompte, "Condition d'accès incorrecte");
+		verifier(ressourcesLEP,LEP.montantRessources,"montant de ressources incorrect");
+	}
+	/**
+	 * Lancement des tests
 	 * 
 	 * @param args
 	 *            inutile
@@ -33,5 +87,4 @@ public class TestBanque {
 	public static void main(String[] args) {
 		LanceurProjet.lanceAvecInterface(new TestBanque());
 	}
-
 }
--- TypeCompte.java
+++ TypeCompte.java
@@ -6,77 +6,55 @@
 *
 */
 class TypeCompte{
-	//caractéristiques réglementaires des différents livrets
-	final float tauxLA	=1.75;
-	final float tauxLDD	=1.75;
-	final float tauxLEP	=2.25;
-	
-	final int plafondLA	=15300;
-	final int plafondLDD	=6000;
-	final int plafondLEP	=7700;
-
-	final boolean accesLA	=false;
-	final boolean accesLDD	=false;
-	final boolean accesLEP	=true;
-
-	final int ressourcesLA	=0;
-	final int ressourcesLDD	=0;
-	final int ressourcesLEP	=757;
-
 	//compteur de comptes crées pour identification unique par type de compte
 	
 	public static int compteurCC	=0;
 	public static int compteurLA	=0;
-	public static int compteurLEP	=0;
 	public static int compteurLDD	=0;
+	public static int compteurLEP	=0;
 
 	//compteur de comptes général
 
-	public int codeInterne		=0;
+	public static int codeInterne	=0;
 
 	// attributs
 
-	public float taux;
+	public double taux;
 	public boolean plafondDepot;
 	public int montantPlafond;	
 	public boolean accesCompte;
 	public int montantRessources;
 	
 	/**
-	*constructeurs de comptes
+	*constructeurs des types de comptes
 	*/
-
-	// compte courant :
-	private static TypeCompte(){
-		compteurCC++;
-		codeInterne++;
-		this.taux=0;
-		this.plafondDepot=false;
-		this.montantDepot=0;
-		this.accesCompte=false;
-		this.montantRessources=0;
+	public TypeCompte(){
+		//Compte Courant
+		this.taux		=0;
+		this.plafondDepot	=false;
+		this.montantPlafond	=0;
+		this.accesCompte	=false;
+		this.montantRessources	=0;
 	}
 	// Autres comptes :
-	private static TypeCompte(float t,boolean p,int m,boolean a,int r,int c){
-		switch(c){
-		case 1
-			compteurLA++;
-			break;
-		case 2
-			compteurLEP++;			
-			break;
-		case 3
-			compteurLEP++;
-			break;
-		default
-			break;
-		}		
-		compteur
-		codeInterne++;
-		this.taux=t;
-		this.plafondDepot=p;
-		this.montantDepot=m
-		this.accesCompte=a;
-		this.montantRessources=r;
+	public TypeCompte(double t,boolean p,int m,boolean a,int r){
+		this.taux		=t;
+		this.plafondDepot	=p;
+		this.montantPlafond	=m;
+		this.accesCompte	=a;
+		this.montantRessources	=r;
 	}
-}		
+	/**
+	* methode par laquelle un type de compte indique la somme
+	* maximale qui peut etre depose
+	* @return plafond de depot (Double.MAX_VALUE si "illimite")
+	*/
+
+	/**
+	* methode par laquelle un type de compte indique le plafond
+	* maximal de revenus (en fait d’impot sur le revenu)
+	* impose a l’ouverture du compte
+	* @return plafond de revenus (Double.MAX_VALUE si "illimite")
+	*/
+
+}
